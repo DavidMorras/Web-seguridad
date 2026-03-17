1b7b398aa64704145ff85768cbf9cf7cf6897c79
David <davidmorras2@gmail.com>
2026-03-17 16:55:59 +0100

    cambio de ubicacion

--- components/footer.tsx
+++ components/footer.tsx
@@ -68,7 +68,7 @@ export function Footer({ setActiveTab }: FooterProps) {
                 LinkedIn
               </a>
               <p className="text-sm text-primary-foreground/80">
-                Madrid, España
+                <Zaragoza></Zaragoza>, España
               </p>
             </div>
           </div>

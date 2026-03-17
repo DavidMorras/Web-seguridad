e34dd052497a8ea8031e94c4b3730bc51f1b4c7f
David <davidmorras2@gmail.com>
2026-03-17 19:13:58 +0100

    Cambio error escroll

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -6,7 +6,7 @@ export default function ScrollToTop() {
   const pathname = usePathname();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
